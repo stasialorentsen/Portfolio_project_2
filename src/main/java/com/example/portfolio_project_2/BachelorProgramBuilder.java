94e1e2c04f3798d5219b231f4da39443f8d268d3
stasialorentsen <stasia.lorentsen@gmail.com>
2024-04-05 23:39:25 +0200

    studyActivityBox implemented, StudentProgram implemented

--- src/main/java/com/example/portfolio_project_2/BachelorProgramBuilder.java
+++ src/main/java/com/example/portfolio_project_2/BachelorProgramBuilder.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,8 +19,13 @@ public class BachelorProgramBuilder extends Application {
     // ComboBoxes for displaying bachelor programs and their associated study activities
     private ComboBox<String> bachelorProgramBox;
     private ComboBox<String> studyActivityBox;
+    // Add Button
+    private Button addButton;
+    // Text Area
+    private TextArea activityAdded;
     // Map to store program names and their associated IDs
     private Map<String, Integer> programsMap;
+    private Map<String, Integer> activitiesMap;
 
     @Override
     public void start(Stage stage) {
@@ -26,14 +34,15 @@ public class BachelorProgramBuilder extends Application {
         stage.show();
     }
 
-    // This method is responsible for setting up the stage i.e., creating the ComboBoxes,
-    // fetching initial data, setting up listeners, and defining layout of the GUI
+    // This method is responsible for setting up the stage - layout/GUI
     private void setupStage(Stage stage) {
         bachelorProgramBox = new ComboBox<>();
         studyActivityBox = new ComboBox<>();
+        activityAdded = new TextArea();
+        addButton = new Button("Add");
 
         // Fetch base programs from the database and populate the program map and bachelorProgramBox
-        programsMap = fetchDataFromDatabase("SELECT program_id, name FROM BachelorProgram");
+        programsMap = fetchBachelorPrograms("SELECT program_id, name FROM BachelorProgram");
         bachelorProgramBox.getItems().addAll(programsMap.keySet());
 
         // Add an event listener to the bachelorProgramBox to react when a new program is selected
@@ -42,9 +51,28 @@ public class BachelorProgramBuilder extends Application {
             updateStudyActivityBox(newProgram);
         });
 
-        // Create a VBox layout and add the ComboBoxes to it
-        VBox root = new VBox(bachelorProgramBox, studyActivityBox);
-        Scene scene = new Scene(root, 500, 500);
+        addButton.setOnAction(event -> {
+            Integer activityId = activitiesMap.get(studyActivityBox.getValue());
+            Integer programId = programsMap.get(bachelorProgramBox.getValue());
+            addActivityToDatabase(programId, activityId);
+            activityAdded.appendText(studyActivityBox.getValue() + "\n");
+        });
+
+        // Set up GridPane
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10, 10, 10, 10)); // Add some padding
+        gridPane.setVgap(5); // Add vertical gap
+        gridPane.setHgap(5); // Add horizontal gap
+
+        // Add components to GridPane
+        // Parameters are: Node, columnIndex, rowIndex, colSpan, rowSpan
+        gridPane.addRow(0, bachelorProgramBox);
+        gridPane.addRow(1, studyActivityBox);
+        gridPane.addRow(2, addButton);
+        gridPane.addRow(3, activityAdded);
+
+
+        Scene scene = new Scene(gridPane, 500, 500);
         stage.setTitle("BachelorProgramBuilder");
         stage.setScene(scene);
     }
@@ -56,20 +84,54 @@ public class BachelorProgramBuilder extends Application {
     private void updateStudyActivityBox(String programName) {
         // Clear the previous study activities
         studyActivityBox.getItems().clear();
-
         // Fetch the ID for the selected program from the HashMap
         Integer programId = programsMap.get(programName);
-
-        // Fetch study activities for the selected program from the database
-        List<String> studyActivities = fetchDataActivitiesFromDatabase("SELECT name FROM StudyActivity WHERE program_id = " + programId);
-
+        // Fetch study activities for the selected program from the database and update activitiesMap
+        String sqlQuery = "SELECT activity_id, name FROM StudyActivity WHERE program_id = " + programId;
+        activitiesMap = fetchStudyActivities(sqlQuery);
+        List<String> studyActivities = new ArrayList<>(activitiesMap.keySet());
         // Add the new study activities to the studyActivityBox
         studyActivityBox.getItems().addAll(studyActivities);
+    }
+    private Map<String, Integer> activitiesMap(Integer activityId) {
+        Map<String, Integer> data = new HashMap<>();
+        String url = "jdbc:sqlite:identifier.sqlite";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT type_id, ects FROM StudyActivity WHERE activity_id = " + activityId)){
+            while (rs.next()) {
+                int typeId = rs.getInt("type_id");
+                int ects = rs.getInt("ects");
+                data.put("type_id", typeId);
+                data.put("ects", ects);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    private void addActivityToDatabase(Integer programId, Integer activityId) {
+        Map<String, Integer> activityInfo = activitiesMap(activityId);
+
+        String url = "jdbc:sqlite:identifier.sqlite";
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO StudentProgram (program_id, activity_id, type_id, ects) VALUES (?, ?, ?, ?)");) {
+            pstmt.setInt(1, programId);
+            pstmt.setInt(2, activityId);
+            pstmt.setInt(3, activityInfo.get("type_id"));
+            pstmt.setInt(4, activityInfo.get("ects"));
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     // This method fetches the bachelor program names and their IDs from the database.
     // A map of program names and their IDs is returned
-    private Map<String, Integer> fetchDataFromDatabase(String query) {
+    private Map<String, Integer> fetchBachelorPrograms(String query) {
         Map<String, Integer> data = new HashMap<>();
         String url = "jdbc:sqlite:identifier.sqlite";
 
@@ -93,18 +155,20 @@ public class BachelorProgramBuilder extends Application {
 
     // This method fetches the study activities of a certain bachelor program.
     // It returns a list of study activity names.
-    private List<String> fetchDataActivitiesFromDatabase(String query) {
-        List<String> data = new ArrayList<>();
+    private Map<String, Integer> fetchStudyActivities(String query) {
+        Map<String, Integer> data = new HashMap<>();
         String url = "jdbc:sqlite:identifier.sqlite";
 
         try (Connection conn = DriverManager.getConnection(url);
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(query)) {
             while (rs.next()) {
-                String name = rs.getString("name");
+                // Fetch the activity ID and name
+                int activityId = rs.getInt("activity_id");
+                String activityName = rs.getString("name");
 
-                // Add only the activity name to the list
-                data.add(name);
+                // Add to data map
+                data.put(activityName, activityId);
             }
         } catch (SQLException e) {
             e.printStackTrace();
